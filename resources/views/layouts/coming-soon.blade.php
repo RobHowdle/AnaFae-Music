c3a5a0c1a340a2e147fa5fe08a2c143f94cf9f7f
Rob Howdle <robhowdle94@gmail.com>
2026-02-07 21:10:55 +0000

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/coming-soon.blade.php
+++ resources/views/layouts/coming-soon.blade.php
@@ -5,6 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    @php($faviconVersion = @filemtime(public_path('favicon.ico')) ?: time())
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}?v={{ $faviconVersion }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v={{ $faviconVersion }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v={{ $faviconVersion }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v={{ $faviconVersion }}" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}?v={{ $faviconVersion }}" />
+
     <title>{{ $title ?? config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
